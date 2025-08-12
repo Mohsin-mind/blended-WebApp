8872a760aca451b7db23ab52a093705ca2ba0e6e
Mohsin-mind <mohsin.sunasara@mindinventory.com>
2025-08-12 18:26:27 +0530

    feat: initial setup

---
+++ vite.config.js
@@ -0,0 +1,28 @@
+import path from 'path';
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  
+  return {
+  plugins: [react()],
+  server: {
+    host: env.HOST || '0.0.0.0',
+    port: parseInt(env.VITE_PORT || '3001', 10),
+    allowedHosts: ['*'],
+    hmr: {
+      overlay: false,
+    },
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+  build: {
+    minify: true,
+  },
+  resolve: {
+    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
+  },
+  };
+});
